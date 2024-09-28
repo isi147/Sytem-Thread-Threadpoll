d82ab7c660bfcb278e6641c7441b4a075766e389
Seyidmammadli_i <Seyidmammadli_i@STHQ0125-01>
2024-09-28 16:21:50 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+
+int operations = 500;
+var watch = new Stopwatch();
+
+
+
+
+watch.Start();
+UseThread(operations);
+watch.Stop();
+
+Console.WriteLine("Thread Milliseconds :" + watch.ElapsedMilliseconds);
+
+watch.Reset();
+
+
+watch.Start();
+UseThreadPool(operations);
+watch.Stop();
+
+
+Console.WriteLine("ThreadPool Milliseconds :" + watch.ElapsedMilliseconds);
+
+
+void UseThread(int operation)
+{
+    using var countdown = new CountdownEvent(operation);
+    Console.WriteLine("Threads !!!");
+
+    for (int i = 0; i < operation; i++)
+    {
+        var thread = new Thread(() =>
+        {
+            Console.Write($"{Thread.CurrentThread.ManagedThreadId} ");
+
+
+            countdown.Signal();
+        });
+
+        thread.Start();
+    }
+
+    countdown.Wait();
+    Console.WriteLine();
+}
+
+
+
+void UseThreadPool(int operation)
+{
+    using var countdown = new CountdownEvent(operation);
+
+    Console.WriteLine("ThreadPools !!!");
+
+    for (int i = 0; i < operation; i++)
+    {
+        ThreadPool.QueueUserWorkItem(_ =>
+        {
+            Console.Write($"{Thread.CurrentThread.ManagedThreadId} ");
+
+
+            countdown.Signal();
+        });
+    }
+
+    countdown.Wait();
+    Console.WriteLine();
+}
